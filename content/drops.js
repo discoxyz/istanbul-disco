4c2b0925a753d52125b64b56f2c085543c0966dc
Carl Fairclough <hi@carlfairclough.me>
2023-07-23 19:39:23 +0100

    update attestationNation path

--- content/drops.js
+++ content/drops.js
@@ -1,4 +1,3 @@
-
 const yourDate = new Date();
 const currentDate = yourDate.toISOString().split("T")[0].toString();
 
@@ -10,98 +9,114 @@ export const drops = [
     expired: false,
     dropName: "Mashas drop",
     dropPath: "masha_at_celo",
-    schema: "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
+    schema:
+      "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
     credentialSubject: {
       eventName: "Masha at Disco",
       eventDate: "2023-07-19",
     },
-  },  {
+  },
+  {
     dropId: 2,
     hidden: false,
     secretLink: false,
     expired: false,
     dropName: "Carl's drop",
     dropPath: "carl_yo",
-    schema: "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
+    schema:
+      "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
     credentialSubject: {
       eventName: "Carl at Disco",
       eventDate: "2023-07-19",
     },
-  }, {
+  },
+  {
     dropId: 3,
     hidden: false,
     secretLink: false,
     expired: false,
     dropName: "Evin IRL",
     dropPath: "evin_at_ethCC23",
-    schema: "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
+    schema:
+      "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
     credentialSubject: {
       eventName: "Evin IRL @ ETHCC 2023",
       eventDescription: "You met Evin IRL at ETHCC 2023",
       eventDate: "2023-07-17",
     },
-  },{
+  },
+  {
     dropId: 4,
     hidden: false,
     secretLink: false,
     expired: false,
     dropName: "Masha IRL",
     dropPath: "masha_at_ethCC23",
-    schema: "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
+    schema:
+      "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
     credentialSubject: {
       eventName: "Masha IRL @ ETHCC 2023",
       eventDescription: "You met Masha IRL at ETHCC 2023",
       eventDate: "2023-07-17",
     },
-  },{
+  },
+  {
     dropId: 5,
     hidden: false,
     secretLink: false,
     expired: false,
     dropName: "Carmen IRL",
     dropPath: "carmen_at_ethCC23",
-    schema: "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
+    schema:
+      "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
     credentialSubject: {
       eventName: "Carmen IRL @ ETHCC 2023",
       eventDescription: "You met Carmen IRL at ETHCC 2023",
       eventDate: "2023-07-17",
     },
-  },{
+  },
+  {
     dropId: 6,
     hidden: false,
     secretLink: false,
     expired: false,
     dropName: "CoOperate",
     dropPath: "celo_cooperate",
-    schema: "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
+    schema:
+      "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
     credentialSubject: {
       eventName: "CoOperate",
       eventDescription: "Attended CoOperate Paris Mobile Day",
       eventDate: "2023-07-19",
     },
-  },{
+  },
+  {
     dropId: 7,
     hidden: false,
     secretLink: false,
     expired: false,
     dropName: "LDF IRL",
     dropPath: "i_met_ldf",
-    schema: "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
+    schema:
+      "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
     credentialSubject: {
       eventName: "LDF IRL",
       eventDate: currentDate,
     },
-  },{
+  },
+  {
     dropId: 8,
     hidden: false,
     secretLink: false,
     expired: false,
     dropName: "AttestationNation Dinner",
-    dropPath: "attestnation",
-    schema: "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
+    dropPath: "attestationnation",
+    schema:
+      "https://raw.githubusercontent.com/discoxyz/disco-schemas/main/json/AttendanceCredential/1-0-0.json",
     credentialSubject: {
       eventName: "AttestationNation Dinner",
-      eventDescription: "Participated in Disco’s Inaugural AttesationNation Dinner in Paris alongside Optimism, EAS, Gitcoin, PolygonID, District Labs, Lit Protocol, and XMTP",
+      eventDescription:
+        "Participated in Disco’s Inaugural AttesationNation Dinner in Paris alongside Optimism, EAS, Gitcoin, PolygonID, District Labs, Lit Protocol, and XMTP",
       eventDate: "2023-07-16",
     },
   },
