38cc5cb2823c1ed3021b0ea10c0e184e38b2a248
Carl Fairclough <hi@carlfairclough.me>
2023-08-28 18:56:45 +0100

    remove unused imports

--- app/my-drops/[path]/page.tsx
+++ app/my-drops/[path]/page.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
 import { DropView } from "../../../views/drop";
-import { PrismaClient } from "@prisma/client";
-type Props = {
-  params: { path: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
 
-const prisma = new PrismaClient();
-
-export async function generateMetadata(
-): Promise<Metadata> {
-
-
+export async function generateMetadata(): Promise<Metadata> {
   return {
     title: "My drops",
   };
