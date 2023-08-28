0eca7e6202d51931ca7b21d275e69fee142a3ec5
Carl Fairclough <hi@carlfairclough.me>
2023-08-28 16:24:27 +0100

    add tweet link to drops page

--- app/my-drops/[path]/manage/page.tsx
+++ app/my-drops/[path]/manage/page.tsx
@@ -9,6 +9,7 @@ import { ClaimList } from "../../../../components/v2/claimList";
 import { DropForm } from "../../../../components/v2/dropForm";
 import { Credential } from "../../../../components/v2/credCard";
 import { ToastSuccess } from "../../../../components/v2/toast";
+import Link from "next/link";
 // import { useRouter } from "next/navigation";
 // import { recoverMessageAddress } from "viem";
 
@@ -68,9 +69,6 @@ export default function Page() {
 
   return (
     <>
-      <Head>
-        <title>My page title</title>
-      </Head>
       <main className="items-star relative mx-auto mb-auto flex w-full max-w-7xl items-start p-4">
         <div className="mr-12 w-full max-w-3xl text-xl">
           {drop && (
@@ -125,9 +123,27 @@ export default function Page() {
                 />
               </svg>
             </button>
-            <button className="flex h-12 w-12 rounded-full bg-[#1DA1F2]">
-              T
-            </button>
+            <Link
+              className="group flex h-12 w-12 items-center rounded-full bg-[#1DA1F2] p-3"
+              href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+                `Claim your ${name} credential at Disco 🪩
+${process.env.NEXT_PUBLIC_VERCEL_URL}/${path}`,
+              )}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                // xml:space="preserve"
+                viewBox="0 0 248 204"
+                className="opacity-50 transition-all group-hover:scale-105 group-hover:opacity-100"
+              >
+                <path
+                  fill="#fff"
+                  d="M221.95 51.29c.15 2.17.15 4.34.15 6.53 0 66.73-50.8 143.69-143.69 143.69v-.04c-27.44.04-54.31-7.82-77.41-22.64 3.99.48 8 .72 12.02.73 22.74.02 44.83-7.61 62.72-21.66-21.61-.41-40.56-14.5-47.18-35.07 7.57 1.46 15.37 1.16 22.8-.87-23.56-4.76-40.51-25.46-40.51-49.5v-.64c7.02 3.91 14.88 6.08 22.92 6.32C11.58 63.31 4.74 33.79 18.14 10.71c25.64 31.55 63.47 50.73 104.08 52.76-4.07-17.54 1.49-35.92 14.61-48.25 20.34-19.12 52.33-18.14 71.45 2.19 11.31-2.23 22.15-6.38 32.07-12.26-3.77 11.69-11.66 21.62-22.2 27.93 10.01-1.18 19.79-3.86 29-7.95-6.78 10.16-15.32 19.01-25.2 26.16z"
+                />
+              </svg>
+            </Link>
           </div>
           <ClaimList drop={drop} />
         </div>
