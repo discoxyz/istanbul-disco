950f9d0c5ba12fb01236584e1a85cce9ef95018b
Aldi Gjoka <aldigjo@Aldis-MacBook-Pro.local>
2024-01-20 17:05:24 -0800

    Move getClaimstatus

---
+++ lib/api/getClaimStatus.ts
@@ -0,0 +1,45 @@
+import { parseId } from "../validation";
+
+
+export async function getClaimStatus(args: {
+  owner: string;
+  claimant: string;
+}): Promise<{ claimed: boolean }> {
+  const person1 = parseId(args.owner);
+  const person2 = parseId(args.claimant);
+
+  const response = await fetch(`https://api.disco.xyz/v1/credentials/search`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.DISCO_KEY}`,
+    },
+    body: JSON.stringify({
+      conjunction: "and",
+      criteria: [
+        {
+          field: "issuer",
+          operator: "=",
+          value: process.env.ISSUER_DID,
+        },
+        {
+          field: "vc.credentialSubject.person1",
+          operator: "=",
+          value: person1,
+        },
+        {
+          field: "vc.credentialSubject.person2",
+          operator: "=",
+          value: person2,
+        },
+      ],
+      page: 1,
+      size: 1,
+    }),
+  });
+  const credentials = await response.json();
+  return {
+    claimed: !!credentials.length,
+  };
+}
+
